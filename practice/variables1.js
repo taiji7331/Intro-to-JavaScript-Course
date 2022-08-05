c70ebcaff0d6646dd1b7a75c210202106d59ef52
Taiji7331 <110615435+taiji7331@users.noreply.github.com>
2022-08-05 23:37:36 +0900

    Adding comments explaining concatenation

--- practice/variables1.js
+++ practice/variables1.js
@@ -10,7 +10,10 @@ console.log("My pet cat's name is: " + petCat);
 var catSound = "purr";
 var dogSound = "woof";
 
+// I wanted to use both ways of concatenating strings to see how their behavior differs
+// Traditional concatenation: using the + operator. In this case, spaces must be added to string literals manually.
 console.log(petDog + " says " + dogSound);
+// Comma delineated lists: This seems to have the behavior of automatically adding spaces between items. 
 console.log(petCat, "says", catSound);
 
 catSound = "meow";
