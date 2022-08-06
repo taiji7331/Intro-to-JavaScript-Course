e4e5da7561d5057b6ee5350f8f9e47931af1fa5f
Taiji7331 <110615435+taiji7331@users.noreply.github.com>
2022-08-06 23:15:34 +0900

    Added loop practice file

---
+++ practice/loops1.js
@@ -0,0 +1,41 @@
+// Practice for loops and while loops
+
+// For loops
+// Counting up
+for (i = 1; i <= 5; i++) {
+    console.log(i);
+}
+console.log("Counting completed!");
+
+// Counting down
+for (i = 5; i > 0; i--) {
+    console.log(i);
+}
+console.log("Countdown finished!");
+
+// While loops
+// Counting up
+var i = 1;
+
+while(i < 6) {
+    console.log(i);
+    i++;
+}
+console.log("Counting completed!");
+
+// Counting down
+var i = 5;
+
+while(i > 0) {
+    console.log(i);
+    i = i - 1;
+}
+console.log("Countdown finished!");
+
+// Counting years
+var year = 2018;
+
+while(year < 2023) {
+    console.log(year);
+    year++;
+}
