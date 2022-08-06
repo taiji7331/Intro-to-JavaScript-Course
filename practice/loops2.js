7740933946c0b27a931d204fe1d8573f2a8e73d5
Taiji7331 <110615435+taiji7331@users.noreply.github.com>
2022-08-06 23:38:34 +0900

    Added second loops practice file
    Using conditionals inside loops

---
+++ practice/loops2.js
@@ -0,0 +1,31 @@
+// More loops practice
+
+// If/else statement
+for(i = 1; i <= 10; i++) {
+    if (i == 1) {
+        console.log("Gold medal");
+    } else if (i == 2) {
+        console.log("Silver medal");
+    } else if (i == 3) {
+        console.log("Bronze medal");
+    } else {
+        console.log(i);
+    }
+}
+
+// Switch statement
+for(i = 1; i<=10; i++) {
+    switch(i) {
+        case 1:
+            console.log("Gold medal");
+            break;
+        case 2: 
+            console.log("Silver medal");
+            break;
+        case 3:
+            console.log("Bronze medal");
+            break;
+        default:
+            console.log(i);
+    }
+}
