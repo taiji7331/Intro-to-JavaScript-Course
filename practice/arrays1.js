04b82b70b03a76e025704206f6b2257b82ffb229
Taiji7331 <110615435+taiji7331@users.noreply.github.com>
2022-08-07 14:49:02 +0900

    Added arrays practice file

---
+++ practice/arrays1.js
@@ -0,0 +1,24 @@
+// Practice with arrays and objects in JavaScript
+
+// Simple array built using the push method
+var clothes = [];
+clothes.push("pants");
+clothes.push("shirt");
+clothes.push("hat");
+clothes.push("jacket");
+clothes.push("shoes");
+
+// Removing items using pop and adding more with push
+clothes.pop();
+clothes.push("socks");
+
+// Displaying specific items from an array
+console.log(clothes[2]);
+
+// Simple object built using dot notation
+var favCar = {};
+favCar.color = "blue";
+favCar.convertible = false;
+
+// Displaying the entire object
+console.log(favCar);
